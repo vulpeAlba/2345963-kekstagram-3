db4d9eb2d38e241e043e180944938874dc10981f
Viktoria Anufrieva <anufrievavick@gmai.com>
2023-03-16 22:39:12 +0300

    New version of randNum

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 function randNum (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max))); //округление до ближайшего большего
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max))); //округление до ближайшего меньшего
+  return Math.floor(Math.random() * (upper - lower + 1)) + lower;
 }
 
 function strLenCheck (str, maxLength) {
   return str.length <= maxLength;
 }
 
-randNum(2, 5);
+randNum(2, 8);
 strLenCheck('Hello, World', 15);
