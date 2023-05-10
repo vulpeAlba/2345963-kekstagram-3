cb459cf21c6f8f12d99094ec19f12c15a813541a
Viktoria Anufrieva <anufrievavick@gmai.com>
2023-05-11 01:40:11 +0300

    module8_task1-2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
-import { strLenCheck } from './util.js';
 import { createPostsList } from './data.js';
 import { render } from './script.js';
 import './validate.js';
 import './form.js';
 
-strLenCheck('Hello, World', 15);
 const data = createPostsList(25);
 render(data);
